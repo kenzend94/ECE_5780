e8496991055aba2a9670634a8dce3ab9b4c324c9
kenzend94 <56566535+kenzend94@users.noreply.github.com>
2024-02-09 13:10:32 -0700

    done lab2

--- Lab2/Core/Src/main.c
+++ Lab2/Core/Src/main.c
@@ -47,7 +47,10 @@ int main(void)
   NVIC_EnableIRQ(EXTI0_1_IRQn);
 
   // Set the priority for the interrupt to 1 (high-priority) with the NVIC_SetPriority() function.
-  NVIC_SetPriority(EXTI0_1_IRQn, 1);
+  NVIC_SetPriority(EXTI0_1_IRQn, 3);
+
+  // changes the SysTick interrupt priority to 2 (medium priority)
+  // NVIC_SetPriority(SysTick_IRQn, 2);
 
   // clear mode bits for PC6, 7, 8, 9
   GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk |
@@ -97,105 +100,6 @@ int main(void)
     GPIOC->ODR ^= GPIO_ODR_6; // Toggle PC6
     HAL_Delay(500);           // Delay for 500 milliseconds
   }
-
-  // // green and orange LEDs are on pins PC8 & 9
-  // // red and blue LEDs are on pins PC6 & 7
-
-  // // // clear mode bits for PC8 & 9
-  // // GPIOC->MODER &= ~(GPIO_MODER_MODER8_Msk | GPIO_MODER_MODER9_Msk);
-  // // clear mode bits for PC6 & 7
-  // GPIOC->MODER &= ~(GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk);
-
-  // // config GPIOC PC8 & PC9 as general-purpose output mode
-
-  // // // set mode bits to 01 for PC8 & 9
-  // // GPIOC->MODER |= (GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
-  // // set mode bits to 01 for PC6 & 7
-  // GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0);
-
-  // // // config PC8 and PC9 as push-pull output type
-  // // GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);
-  // // config PC6 and PC7 as push-pull output type
-  // GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7);
-
-  // // // config PC8 and PC9 to low speed
-  // // GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR8_Msk | GPIO_OSPEEDR_OSPEEDR9_Msk);
-  // // config PC6 and PC7 to low speed
-  // GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR6_Msk | GPIO_OSPEEDR_OSPEEDR7_Msk);
-
-  // // // config PC8 and PC9 with no pull-up/down resistors
-  // // GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8_Msk | GPIO_PUPDR_PUPDR9_Msk);
-  // // config PC6 and PC7 with no pull-up/down resistors
-  // GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6_Msk | GPIO_PUPDR_PUPDR7_Msk);
-
-  // // Initialize one pin logic high and the other to low.
-  // // GPIOC->BSRR = GPIO_BSRR_BS_8; // Set PC8 high
-  // // GPIOC->BSRR = GPIO_BSRR_BR_9; // Set PC9 low
-
-  // GPIOC->BSRR = GPIO_BSRR_BS_6; // Set PC6 high
-  // GPIOC->BSRR = GPIO_BSRR_BR_7; // Set PC7 low
-
-  // // Configure the button pin to input mode with the internal pull-down resistor enabled.
-  // // Set PA0 to input mode
-  // GPIOA->MODER &= ~(GPIO_MODER_MODER0_Msk);
-  // // Set PA0 to low speed
-  // GPIOA->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR0_Msk);
-  // // Clear pull-up/pull-down bits for PA0
-  // GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR0_Msk;
-  // // Set PA0 to pull-down mode
-  // GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR0_Msk);
-
-  // // Monitor the button pin input state within the endless program loop.
-  // // Use the IDR register.
-
-  // // Initialize the debouncer and LED state
-  // uint32_t debouncer = 0;
-  // uint32_t ledState = 0;
-
-  // while (1)
-  // {
-  //   // Shift the debouncer register and read the button state
-  //   debouncer = (debouncer << 1);
-
-  //   // check if button is set
-  //   int buttonPressed = GPIOA->IDR & 1;
-
-  //   // Check if the button is pressed
-  //   if (buttonPressed)
-  //   {
-  //     // Set lowest bit of bit-vector
-  //     debouncer |= 0x01;
-  //   }
-
-  //   if (debouncer == 0x7FFFFFFF)
-  //   {
-  //     if (ledState == 0)
-  //     {
-  //       GPIOC->BSRR = GPIO_BSRR_BR_6; // Set PC6 low
-  //       GPIOC->BSRR = GPIO_BSRR_BS_7; // Set PC7 high
-  //       ledState = 1;
-  //     }
-  //     else
-  //     {
-  //       GPIOC->BSRR = GPIO_BSRR_BS_6; // Set PC6 high
-  //       GPIOC->BSRR = GPIO_BSRR_BR_7; // Set PC7 low
-  //       ledState = 0;
-  //     }
-  //   }
-
-  // }
-  // HAL_Delay(1);
-
-  // Here is the first part 1.5.1: Configuring a GPIO Pin to Output and Blink an LED
-
-  // while (1)
-  // {
-  //   // Toggle PC6 and PC7 using ODR
-  //   GPIOC->ODR ^= (GPIO_ODR_6 | GPIO_ODR_7); // Toggle PC7 and PC6
-
-  //   // Delay for a while to see the toggling effect
-  //   HAL_Delay(500); // Delay for 500 milliseconds
-  // }
 }
 
 /**
@@ -255,22 +159,15 @@ void Error_Handler(void)
 void EXTI0_1_IRQHandler(void)
 {
   // Check if the interrupt was triggered by EXTI0
-  if (EXTI->PR & EXTI_PR_PR0)
+  EXTI->PR |= EXTI_PR_PR0;
+
+  GPIOC->ODR ^= (GPIO_ODR_8 | GPIO_ODR_9);
+
+  for (volatile uint32_t i = 0; i < 1500000; ++i)
   {
-    static uint32_t counter = 0;
-    counter++;
-    if (counter == 150000)
-    {
-      // Clear the interrupt pending bit for EXTI0
-      EXTI->PR |= EXTI_PR_PR0;
-
-      // Toggle both the green and orange LEDs (PC8 & PC9) in the EXTI interrupt handler.
-      GPIOC->ODR ^= (GPIO_ODR_8 | GPIO_ODR_9);
-
-      GPIOC->ODR ^= GPIO_ODR_7;
-      counter = 0;
-    }
   }
+
+  GPIOC->ODR ^= (GPIO_ODR_8 | GPIO_ODR_9);
 }
 
 #ifdef USE_FULL_ASSERT
